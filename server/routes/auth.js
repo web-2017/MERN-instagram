d617bd8043d241883a29df1a2fba79bc99b0c2e8
web-2017 <zoro987321>
2020-12-30 15:20:27 +0300

    set user hashPassword via bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,33 +1,46 @@
 import express from "express";
 
 const router = express.Router()
+import bcrypt from 'bcryptjs'
 
 import User from "../models/user.js";
 
+router
+    .get('/', (req, res) => {
+        res.json('router hello')
+    })
+    .post('/signup', async (req, res) => {
 
-router.get('/', (req, res) => {
-    res.json('router hello')
-})
+        const {name, email, password} = await req.body
+        if (!email) return res.status(422).json({error: 'Пожалуйста, введите email'})
+        if (!password) return res.status(422).json({error: 'Пожалуйста, введите пароль'})
 
-router.post('/signup', (req, res) => {
-    const {name, email, password} = req.body
-    if (!email) res.status(422).json({error: 'Пожалуйста, введите email'})
-    if (!password) res.status(422).json({error: 'Пожалуйста, введите пароль'})
-    User.findOne({email: email})
-        .then((savedUser) => {
-            if (savedUser) {
-                if (!email) res.status(422).json({error: 'email уже существует'})
-            }
-            const user = new User(req.body)
-            user.save()
-                .then(user => {
-                    res.status(200).json({message: 'Success'})
-                    console.log(`Пользователь ${req.body.email} создан`)
-                })
-                .catch(error => console.log(error))
-        })
-        .catch(error => console.log(error))
+        try {
+            const checkUser = await User.findOne({email: email})
 
-})
+            // Если email уже сушествует, ошибка
+            if (checkUser) return res.status(422).json({error: `Такой email: ${email} - уже существует`})
+
+            // bcrypt password
+            const hashPassword = await bcrypt.hash(req.body.password, 12)
+
+            // Создали нового пользователя
+            const user = new User({
+                name,
+                email,
+                password: hashPassword, // Зашифровали password
+            })
+
+            await user.save()
+
+            res.status(200).json({message: 'Success'})
+            console.log(`Пользователь ${req.body.email} создан`)
+
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    })
 
 export default router
