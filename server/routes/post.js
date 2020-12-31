a96b7502a8830a78bba038deb1a1c95787769727
web-2017 <zoro987321>
2020-12-31 12:06:27 +0300

    all posts route

--- server/routes/post.js
+++ server/routes/post.js
@@ -5,6 +5,17 @@ import requireLogin from "../middleware/requireLogin.js";
 const router = express.Router()
 
 router
+    .get('/allpost', async (req, res) => {
+        /**
+         *  Получили все posts, postedBy
+         *  postedBy - сортировка по полю,
+         *  select - какие поля будут видны
+         */
+        const posts = await Post.find().populate('postedBy', "_id name")
+
+        res.json(posts)
+        console.log(posts)
+    })
     .post('/createpost', requireLogin, (req, res) => {
         const {title, body} = req.body
         if (!title || !body) return res.status(422).json({error: 'Все поля обязательны для заполнения'})
@@ -15,7 +26,6 @@ router
         const post = new Post({title, body, postedBy: req.user})
         post.save()
         res.json(post)
-
     })
 
 export default router
