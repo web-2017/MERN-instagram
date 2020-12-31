3df414872bb2424816ad99c5c8f5bc4eb80e66fe
web-2017 <zoro987321>
2020-12-31 12:24:41 +0300

    All post created by signed In user route

--- server/routes/post.js
+++ server/routes/post.js
@@ -11,21 +11,31 @@ router
          *  postedBy - сортировка по полю,
          *  select - какие поля будут видны
          */
-        const posts = await Post.find().populate('postedBy', "_id name")
-
-        res.json(posts)
-        console.log(posts)
+        try {
+            const posts = await Post.find().populate('postedBy', "_id name")
+            res.json(posts)
+        } catch (e) {
+            console.log(e)
+        }
     })
     .post('/createpost', requireLogin, (req, res) => {
         const {title, body} = req.body
         if (!title || !body) return res.status(422).json({error: 'Все поля обязательны для заполнения'})
 
+        // remove keys
         req.user.password = undefined
         req.user.__v = undefined
 
-        const post = new Post({title, body, postedBy: req.user})
-        post.save()
-        res.json(post)
+        try {
+            const post = new Post({title, body, postedBy: req.user})
+            post.save().then(() => res.json(post))
+        } catch (e) {
+            console.log(e)
+        }
+    })
+    .get('/mypost', requireLogin, async (req, res) => {
+        const posts = await Post.find({postedBy: req.user._id}).populate('postedBy', "_id name")
+        res.json(posts)
     })
 
 export default router
