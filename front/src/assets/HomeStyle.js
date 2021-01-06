65766d37aa4d2e355f09876a69be4b5f7c5b3e87
web-2017 <zoro987321>
2021-01-06 09:44:11 +0300

    Creating Home page UI

---
+++ front/src/assets/HomeStyle.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+    
+`
+export const HomeCard = styled.div`
+  max-width: 500px;
+  height: max-content;
+  margin: 26px auto;
+`
+export const CardImage = styled.div`
+  
+`
+export const CardContent = styled.div`
+  padding: 20px 40px;
+`
--- front/src/screens/Home.js
+++ front/src/screens/Home.js
@@ -1,7 +1,49 @@
 import React from "react";
 
-const Home = () => (
-    <h1>Heelo Home page</h1>
-)
+import {HomeContainer, HomeCard, CardImage, CardContent} from "../assets/HomeStyle";
+
+const Home = () => {
+    const urlImage = 'https://images.unsplash.com/photo-1598128558393-70ff21433be0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fHdhbGxwZXJzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    return (
+        <HomeContainer>
+            <HomeCard className="card">
+                <h5>Ivan</h5>
+                <CardImage>
+                    <img src={urlImage} alt=""/>
+                </CardImage>
+                <CardContent>
+                    <i className="Small material-icons">favorite</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder='add comment'/>
+                </CardContent>
+            </HomeCard>
+            <HomeCard className="card">
+                <h5>Ivan</h5>
+                <CardImage>
+                    <img src={urlImage} alt=""/>
+                </CardImage>
+                <CardContent>
+                    <i className="Small material-icons">favorite</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder='add comment'/>
+                </CardContent>
+            </HomeCard>
+            <HomeCard className="card">
+                <h5>Ivan</h5>
+                <CardImage>
+                    <img src={urlImage} alt=""/>
+                </CardImage>
+                <CardContent>
+                    <i className="Small material-icons">favorite</i>
+                    <h6>title</h6>
+                    <p>this is a post</p>
+                    <input type="text" placeholder='add comment'/>
+                </CardContent>
+            </HomeCard>
+        </HomeContainer>
+    )
+}
 
 export default Home
