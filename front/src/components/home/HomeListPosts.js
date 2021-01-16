c2e3e74114290069e1ad416c86bec5e60aed695b
web-2017 <zoro987321>
2021-01-16 21:30:03 +0300

    Вынес разметку Home

---
+++ front/src/components/home/HomeListPosts.js
@@ -0,0 +1,62 @@
+import React, {useContext} from "react";
+import {CardContent, CardImage, HomeCard} from "../../assets/HomeStyle";
+import {UserContext} from "../../App";
+
+export default ({post, deletePost, likePostHandler, unLikePostHandler, makeComment}) => {
+    const {state} = useContext(UserContext)
+    return (
+        <HomeCard className="card flex space-between">
+            <div className="row">
+                <div className="col flow-text">
+                    <span>{post.postedBy.name}</span>
+                </div>
+
+                <div className="col flow-text">
+                    {
+                        post.postedBy._id == state.id
+                        &&
+                        <span><i className="material-icons" onClick={() => deletePost(post._id)}>delete</i></span>
+                    }
+                </div>
+
+            </div>
+            <CardImage className='card-image'>
+                <img style={{width: '100%'}} src={post.image} alt=""/>
+                <span className="card-title">Card Title</span>
+            </CardImage>
+            <CardContent>
+                <i className="small material-icons red-text">favorite</i>
+                {
+                    post.likes.includes(state.id)
+                        ?
+                        <i className="material-icons"
+                           onClick={() => unLikePostHandler(post._id)}
+                        >thumb_down</i>
+                        : <i className="material-icons"
+                             onClick={() => likePostHandler(post._id)}
+                        >thumb_up</i>
+                }
+                <h6>{post.likes.length} likes</h6>
+                <h6>{post.title}</h6>
+                <p>{post.body}</p>
+                {
+                    post.comments.map(comment => {
+                        return (
+                            <h6 key={comment._id}><span
+                                className='text-darken-1'>{comment.postedBy.name}</span> {comment.text}
+                            </h6>
+                        )
+                    })
+                }
+                <form onSubmit={event => {
+                    event.preventDefault()
+                    makeComment(event.target[0].value, post._id)
+                    event.target[0].value = ''
+                }}>
+
+                    <input type="text" placeholder='add comment and press Enter'/>
+                </form>
+            </CardContent>
+        </HomeCard>
+    )
+}
--- front/src/screens/Home.js
+++ front/src/screens/Home.js
@@ -1,15 +1,16 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, {useState, useEffect,} from "react";
 
 import HomeCrudClass from '../components/home/HomeCrudClass'
 
 import {HomeContainer, HomeCard, CardImage, CardContent} from "../assets/HomeStyle";
 
-import {UserContext} from "../App";
+
+import HomeListPosts from "../components/home/HomeListPosts";
 
 const Home = () => {
     const CreateHomeCrud = new HomeCrudClass(`Bearer ${localStorage.getItem('token')}`)
 
-    const {state} = useContext(UserContext)
+
     const [data, setData] = useState([])
 
     useEffect(() => {
@@ -26,7 +27,7 @@ const Home = () => {
      * @param res - result of response.json
      * @returns {[data] | [res]}
      */
-    const newPostData = (prevData = [], res = []) => {
+    const replaceData = (prevData = [], res = []) => {
         const data = prevData.map(item => {
             if (item._id === res._id) {
                 return res
@@ -40,19 +41,19 @@ const Home = () => {
     // like post func
     const likePostHandler = async (id) => {
         const like = await CreateHomeCrud.likePostHandler(id)
-        setData(newPostData(data, like))
+        setData(replaceData(data, like))
     }
 
     // unlike post func
     const unLikePostHandler = async (id) => {
         const unLike = await CreateHomeCrud.unLikePostHandler(id)
-        setData(newPostData(data, unLike))
+        setData(replaceData(data, unLike))
     }
 
     // create comments
     const makeComment = async (text, postId) => {
         const comment = await CreateHomeCrud.makeComment(text, postId)
-        setData(newPostData(data, comment))
+        setData(replaceData(data, comment))
     }
 
     // delete comments
@@ -71,59 +72,14 @@ const Home = () => {
             {
                 data.map(post => {
                     return (
-                        <HomeCard key={post._id} className="card flex space-between">
-                            <div className="row">
-                                <div className="col flow-text">
-                                    <span>{post.postedBy.name}</span>
-                                </div>
-
-                                <div className="col flow-text">
-                                    {post.postedBy._id == state.id
-                                    && <span>
-                                        <i className="material-icons"
-                                           onClick={() => deletePost(post._id)}
-                                        >delete</i></span>}
-                                </div>
-
-                            </div>
-                            <CardImage className='card-image'>
-                                <img style={{width: '100%'}} src={post.image} alt=""/>
-                                <span className="card-title">Card Title</span>
-                            </CardImage>
-                            <CardContent>
-                                <i className="small material-icons red-text">favorite</i>
-                                {
-                                    post.likes.includes(state.id)
-                                        ?
-                                        <i className="material-icons"
-                                           onClick={() => unLikePostHandler(post._id)}
-                                        >thumb_down</i>
-                                        : <i className="material-icons"
-                                             onClick={() => likePostHandler(post._id)}
-                                        >thumb_up</i>
-                                }
-                                <h6>{post.likes.length} likes</h6>
-                                <h6>{post.title}</h6>
-                                <p>{post.body}</p>
-                                {
-                                    post.comments.map(comment => {
-                                        return (
-                                            <h6 key={comment._id}><span
-                                                className='text-darken-1'>{comment.postedBy.name}</span> {comment.text}
-                                            </h6>
-                                        )
-                                    })
-                                }
-                                <form onSubmit={event => {
-                                    event.preventDefault()
-                                    makeComment(event.target[0].value, post._id)
-                                    event.target[0].value = ''
-                                }}>
-
-                                    <input type="text" placeholder='add comment and press Enter'/>
-                                </form>
-                            </CardContent>
-                        </HomeCard>
+                        <HomeListPosts
+                            key={post._id}
+                            post={post}
+                            eletePost={deletePost}
+                            likePostHandler={likePostHandler}
+                            unLikePostHandler={unLikePostHandler}
+                            makeComment={makeComment}
+                        />
                     )
                 })
             }
